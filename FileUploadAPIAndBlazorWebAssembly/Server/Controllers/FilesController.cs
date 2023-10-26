8fb8760c2fb65dc345302df9076cfe878faf199f
yazan.aqel93 <yazan.aqel93@gmail.com>
2023-10-26 19:34:47 +0300

    Add project files.

---
+++ FileUploadAPIAndBlazorWebAssembly/Server/Controllers/FilesController.cs
@@ -0,0 +1,49 @@
+﻿using FileUploadAPIAndBlazorWebAssembly.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Http.Headers;
+
+namespace FileUploadAPIAndBlazorWebAssembly.Server.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class FilesController : ControllerBase
+{
+	private readonly IWebHostEnvironment webHost;
+
+	public FilesController(IWebHostEnvironment webHost)
+	{
+		this.webHost = webHost;
+	}
+
+	[HttpPost]
+	public async Task<ActionResult<List<UploadResult>>> Upload(List<IFormFile> files)
+	{
+		List<UploadResult> uploadResults = new List<UploadResult>();
+
+		foreach (var file in files)
+		{
+			var uploadResult = new UploadResult();
+
+			string trustedFileNameForFileStorage;
+
+			var unTrustedFileName = file.FileName;
+
+			uploadResult.FileName = unTrustedFileName;
+
+			var trustedFileNameForDisplay = WebUtility.HtmlEncode(unTrustedFileName);
+
+			trustedFileNameForFileStorage = Path.GetRandomFileName();
+
+			var path = Path.Combine(webHost.ContentRootPath, "Uploads",trustedFileNameForFileStorage);
+
+			await using FileStream stream = new(path, FileMode.Create);
+			await stream.CopyToAsync(stream);
+
+			uploadResult.StoredFileName = trustedFileNameForFileStorage;
+			uploadResults.Add(uploadResult);
+		}
+
+		return Ok(uploadResults);
+	}
+}
---
+++ FileUploadAPIAndBlazorWebAssembly/Shared/UploadResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUploadAPIAndBlazorWebAssembly.Shared;
+public class UploadResult
+{
+    public string? FileName { get; set; }
+    public string? StoredFileName { get; set; }
+}
